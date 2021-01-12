d68d3fe87d1dfe9ce42f29577b525850a17a9575
veeru <immaculateempyrean@gmail.com>
2021-01-12 13:00:08 +0530

    expanded log api to write to different files based on log level.. this feature is not yet tested. as the entire log library had to be refactored to get this feature working. the previous tests conducted on log are useless and new tests are to be conducted to ensure that no bugs remain.

--- workspace/pengrudium2D/include/Log.h
+++ workspace/pengrudium2D/include/Log.h
@@ -11,9 +11,29 @@ namespace penguin2D
 	{
 	public:
 		consoleLog();
-		operator std::shared_ptr<spdlog::logger>() { return m_handle; };
-		std::shared_ptr<spdlog::logger> get() { return m_handle; };
 
+	public:
+		template<typename... args>
+		static void write(consoleLog logObj,int level,args... logToWrite)
+		{
+			switch (level)
+			{
+			case 0: logObj.m_handle->trace(logToWrite...);
+				break;
+			case 1: logObj.m_handle->info(logToWrite...);
+				break;
+			case 2: logObj.m_handle->warn(logToWrite...);
+				break;
+			case 3: logObj.m_handle->error(logToWrite...);
+				break;
+			case 4: logObj.m_handle->critical(logToWrite...);
+				break;
+			default:
+				logObj.m_handle->critical("logger level is not as expected unable to log at chosen level defaulting to error..");
+				logObj.m_handle->error(logToWrite...);
+				break;
+			}
+		}
 	private:
 		std::shared_ptr<spdlog::logger> m_handle = nullptr;
 	};
@@ -25,8 +45,44 @@ namespace penguin2D
 		operator std::shared_ptr<spdlog::logger>() { return m_handle; };
 		std::shared_ptr<spdlog::logger> get() { return m_handle; };
 
+	public:
+		template<typename... args>
+		static void write(fileLog logObj,int level,args... logToWrite)
+		{
+			switch (level)
+			{
+			case 0:  logObj.m_handle->trace(logToWrite...);
+					 logObj.m_trace_handle->trace(logToWrite...);
+				break;
+
+			case 1:  logObj.m_handle->info(logToWrite...);
+					 logObj.m_info_handle->info(logToWrite...);
+				break;
+			case 2:  logObj.m_handle->warn(logToWrite...);
+					 logObj.m_warn_handle->warn(logToWrite...);
+				break;
+			case 3:  logObj.m_handle->error(logToWrite...);
+					 logObj.m_error_handle->error(logToWrite...);
+				break;
+			case 4:  logObj.m_handle->critical(logToWrite...);
+					 logObj.m_critical_handle->critical(logToWrite...);
+				break;
+			default: logObj.m_handle->critical("logger level is not as expected unable to log at chosen level defaulting to error..");
+					 logObj.m_handle->error(logToWrite...);
+
+					 logObj.m_unclassified_handle->error(logToWrite...);
+				break;
+			}
+		}
 	private:
-		std::shared_ptr<spdlog::logger> m_handle = nullptr;
+		std::shared_ptr<spdlog::logger> m_handle = nullptr; //this is the global file to which all logs are to be written..
+		
+		std::shared_ptr<spdlog::logger> m_trace_handle		= nullptr;
+		std::shared_ptr<spdlog::logger> m_info_handle			= nullptr;
+		std::shared_ptr<spdlog::logger> m_warn_handle			= nullptr;
+		std::shared_ptr<spdlog::logger> m_error_handle		= nullptr;
+		std::shared_ptr<spdlog::logger> m_critical_handle		= nullptr;
+		std::shared_ptr<spdlog::logger> m_unclassified_handle = nullptr;
 	};
 
 	class log
@@ -34,37 +90,15 @@ namespace penguin2D
 	public:
 		static consoleLog getConsoleLogger();
 		static fileLog getFileLogger();
-
-		template<typename T,typename... args>
-		static void write(T logObject, int level, args... logToWrite)
-		{
-			switch (level)
-			{
-			case 0: logObject.get()->trace(logToWrite...);
-				break;
-			case 1: logObject.get()->info(logToWrite...);
-				break;
-			case 2: logObject.get()->warn(logToWrite...);
-				break;
-			case 3: logObject.get()->error(logToWrite...);
-				break;
-			case 4: logObject.get()->critical(logToWrite...);
-				break;
-			default: 
-				logObject.get()->critical("logger level is not as expected unable to log at chosen level defaulting to error..");
-				logObject.get()->error(logToWrite...);
-				break;
-			}
-		}
 	};
 }
 
 #if defined(PE_DEBUG)
-	#define logConsole(level,...)	penguin2D::log::write(penguin2D::log::getConsoleLogger(),level,__VA_ARGS__);
-	#define logFile(level,...)		penguin2D::log::write(penguin2D::log::getFileLogger(),level,__VA_ARGS__);
+	#define logConsole(level,...)	penguin2D::consoleLog::write(penguin2D::log::getConsoleLogger(),level,__VA_ARGS__);
+	#define logFile(level,...)		penguin2D::fileLog::write(penguin2D::log::getFileLogger(),level,__VA_ARGS__);
 	
-	#define log(level,...)			penguin2D::log::write(penguin2D::log::getConsoleLogger(),level,__VA_ARGS__);\
-									penguin2D::log::write(penguin2D::log::getFileLogger()   ,level, __VA_ARGS__);
+	#define log(level,...)			penguin2D::consoleLog::write(penguin2D::log::getConsoleLogger(),level,__VA_ARGS__);\
+									penguin2D::fileLog::write(penguin2D::log::getFileLogger()   ,level, __VA_ARGS__);
 
 #else
 #define consoleLog(level,...)		
--- workspace/pengrudium2D/src/Log.cpp
+++ workspace/pengrudium2D/src/Log.cpp
@@ -26,8 +26,28 @@ namespace penguin2D
 			initializeLog();
 			});
 
-		m_handle = spdlog::basic_logger_mt("defaultFileLogger", "log.txt");
+		m_handle = spdlog::basic_logger_mt("defaultFileLogger", "log/log.txt");
 		m_handle->set_level(spdlog::level::trace);
+
+
+		m_trace_handle = spdlog::basic_logger_mt("defaultTraceLogger",	"log/logTrace.txt");
+		m_trace_handle->set_level(spdlog::level::trace);
+		
+		m_info_handle = spdlog::basic_logger_mt("defaultInfoLogger",		"log/logInfo.txt");
+		m_info_handle->set_level(spdlog::level::info);
+		
+		m_warn_handle = spdlog::basic_logger_mt("defaultWarnLogger",		"log/logWarn.txt");
+		m_warn_handle->set_level(spdlog::level::warn);
+		
+		m_error_handle = spdlog::basic_logger_mt("defaultErrorLogger",	"log/logError.txt");
+		m_error_handle->set_level(spdlog::level::err);
+		
+		m_critical_handle = spdlog::basic_logger_mt("defaultCriticalLogger", "log/logCritical.txt");
+		m_critical_handle->set_level(spdlog::level::critical);
+
+
+		m_unclassified_handle = spdlog::basic_logger_mt("defaultUnspecifiedLogger", "log/logUnspecified.txt");
+		m_unclassified_handle->set_level(spdlog::level::err);
 	}
 	
 
